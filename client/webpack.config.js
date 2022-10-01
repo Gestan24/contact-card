ee12f4533ce43ef845959a2186583423b8c4caa7
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-01 17:28:45 -0500

    added service worker and injectManifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+const path = require('path');
 
 module.exports = {
     mode: 'development',
@@ -9,11 +11,18 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin',
-        })
+        }),
+
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js',
+        }),
+        
     ],
 
     module: {
