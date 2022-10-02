52bcb7e76414d021a82c8cf74c01f221b51c4a81
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-01 20:41:02 -0500

    added installation functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -108,3 +108,22 @@ if ('serviceWorker' in navigator) {
   })
 };
 
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
